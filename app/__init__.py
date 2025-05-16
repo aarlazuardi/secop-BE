f14fc602f09a87f8773d959d1e597378a259c38c
Aar Lazuardi <alazuardi7@gmail.com>
2025-05-16 10:03:28 +0700

    fix: update CORS to allow all Vercel frontend domains

--- app/__init__.py
+++ app/__init__.py
@@ -34,7 +34,8 @@ def create_app() -> Flask:
             "http://127.0.0.1:3000",
             "http://localhost:5173",
             "http://127.0.0.1:5173",
-            "https://shopee-growth-prediction-fe.vercel.app"
+            "https://shopee-growth-prediction-fe.vercel.app",
+            "https://shopee-growth-prediction-fe-git-main-aarlazuardi.vercel.app"
         ],
         supports_credentials=True,
         methods=["GET", "POST", "OPTIONS"],
